8d8634e4dc8b36990e667436a4a691964ec438c3
ViunesC <mrfusking233@gmail.com>
2026-05-09 00:24:39 +0800

    add template

---
+++ app/core/llm.py
@@ -0,0 +1,28 @@
+# LLM client
+# add support to universal models with OpenAI format
+# capable of no-code switch, auto infering provider, fallback
+#
+# Future improvement: Async invoke
+
+class LLMClient:
+
+    def __init__(self) -> None:
+        pass
+
+    def invoke(self):
+        pass
+
+    def think(self):
+        pass
+
+    def invoke_with_tool(self):
+        pass
+
+    async def ainvoke(self):
+        pass
+
+    async def athink(self):
+        pass
+
+    async def ainvoke_with_tool(self):
+        pass
