fe29839323e59d2dcd6f546dfd964de83b158412
ViunesC <mrfusking233@gmail.com>
2026-05-09 15:57:17 +0800

    test run

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from rice-roll-agent!")
+from core.llm import LLMClient
+from dotenv import load_dotenv
 
+load_dotenv()
 
 if __name__ == "__main__":
-    main()
+    llm_client = LLMClient()
+
+    messages = [{"role":"user","content":"What are some of the largest SUVs on the world?"}]
+    for chunk in llm_client.think(messages):
+        print(chunk, end="", flush=True)
+    print()
