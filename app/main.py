0b3c16e37acf78e6bb8bd4631986041bf75167a6
ViunesC <mrfusking233@gmail.com>
2026-05-17 15:34:14 +0800

    test trival agent

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 from core.llm import LLMClient
 from dotenv import load_dotenv
+from agents.trival import TrivalAgent
+from tools.concrete.tarot import get_registry
 
 load_dotenv()
 
 if __name__ == "__main__":
     llm_client = LLMClient()
 
-    messages = [{"role":"user","content":"What are some of the largest SUVs on the world?"}]
-    for chunk in llm_client.think(messages):
-        print(chunk, end="", flush=True)
-    print()
+    alex = TrivalAgent("Alex", llm_client, tools=get_registry())
+    question = "Could do a little 3-card Tarot prediction on my fortune?"
+    
+    print("\n", alex.run(question))
+
+    # print(alex._dump_history())
