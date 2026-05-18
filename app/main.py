dfa7dc8e1e33eda02f353b18d419bdf55fb1ae50
ViunesC <mrfusking233@gmail.com>
2026-05-18 23:17:58 +0800

    test serach tool with plan and solve agent

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from core.llm import LLMClient
 from dotenv import load_dotenv
 from agents.trival import TrivalAgent
 from agents.plan_and_solve import PlanAndSolveAgent
-from tools.concrete.tarot import get_registry
+from tools.concrete.internet_search import get_registry
 
 load_dotenv()
 
@@ -16,11 +16,12 @@ if __name__ == "__main__":
 
     # print(alex._dump_history())
 
-    humphrey = PlanAndSolveAgent("Humphrey", llm_client)
-    task = "There are 4 vacant rooms in Hotel Mossuri on the first night. " \
+    humphrey = PlanAndSolveAgent("Humphrey", llm_client, tools=get_registry())
+    logic_task = "There are 4 vacant rooms in Hotel Mossuri on the first night. " \
     "On the second day there 2 guests moved-in and occupied 1 room. Then on the same day there are 3 guests moved out" \
     "and 2 rooms became vacant. On the third day there are a group of tourists arrived and taken 4 rooms, then there is a family" \
     "moved out and a new room became empty. Question: How many vacant room are there on the fourth day?"
 
-    print("\n", humphrey.run(task))
+    tool_calling_task = "Plan a trip to Toronto, Canada. Give me recommendation in every aspect of a trip."
+    print("\n", humphrey.run(tool_calling_task))
     print(humphrey._dump_history())
